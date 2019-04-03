6bab23a43bc6e2f0ba5c2909e9539e2bcb3b0880
Robert Lee <bobbydwise1@hotmail.com>
2019-04-03 15:42:42 -0700

    Begin adding CRUD.

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
-console.log('May Node be with you')
+app.use(bodyParser.unlencoded({extended: true})
+
+// all your CRUD handlers here
 
 app.listen(3000, function() {
-  console.log('Listening on 3000')
+  console.log('Listening on localhost:3000')
+})
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
+
+app.post('/quotes', (req, res) => {
+  console.log('Hello!!!')
 })
