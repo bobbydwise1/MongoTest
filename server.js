a5ad46c5cd01dd803d3bde379ae8eb3d19a121c4
Robert Lee <bobbydwise1@hotmail.com>
2019-04-03 21:22:18 -0700

    Write basic CREATE into Atlas mongoDB, but no READ.

--- server.js
+++ server.js
@@ -1,19 +1,37 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const MongoClient = require('mongodb').MongoClient
+const uri = 'mongodb+srv://MyMongoDBUser:qwerty123456@cluster0-a32tn.mongodb.net/test?retryWrites=true';
+var db
+
+MongoClient.connect(uri, (err, client) => {
+  if (err) return console.log(err)
+  db = client.db('star-wars-quotes') // whatever your database name is
+  app.listen(3000, () => {
+    console.log('listening on 3000')
+  })
+})
 
 app.use(bodyParser.urlencoded({extended: true}))
 
 // all your CRUD handlers here
 
-app.listen(3000, function() {
-  console.log('Listening on localhost:3000')
-})
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
 })
 
 app.post('/quotes', (req, res) => {
-  console.log(req.body)
+  db.collection('quotes').save(req.body, (err, result) => {
+    if (err) return console.log(err)
+
+    console.log('saved to database')
+    res.redirect('/')
+  })
+})
+
+app.get('/', (req, res) => {
+  var cursor = db.collection('quotes').find()
+  console.log(cursor)
 })
